a46a152c00310f878d9e9d3d858fbeb41e5ee11f
Fergal Walsh <fergalwalsh@gmail.com>
2011-09-05 07:15:37 +0800

    Added the option to run an IPython shell by passing the shell argument
    python -m i2maps.server shell  or python run.py shell  will open a
    python shell with i2maps imported and i2maps.projects_directory set,
    modules in path, etc

--- i2maps/server.py
+++ i2maps/server.py
@@ -2,21 +2,36 @@ import sys
 import i2maps
 import pico
 
-i2maps.projects_directory = ''
+try:
+    from IPython import embed
+except:
+    def embed():
+        print("IPython is required to run the interactive shell!")
+
+i2maps.projects_directory = None
 
 def run():
-    i2maps.modules_directory = i2maps.projects_directory + 'modules/'
-    if i2maps.modules_directory not in sys.path: sys.path.insert(0, i2maps.modules_directory)
+    if i2maps.projects_directory:
+        i2maps.modules_directory = i2maps.projects_directory + 'modules/'
+        if i2maps.modules_directory not in sys.path: sys.path.insert(0, i2maps.modules_directory)
+        views_directory = i2maps.projects_directory + 'views/'
+    else:
+        views_directory = './'
+        i2maps.projects_directory = './'
+    media_directory = i2maps.path + 'media/'
     
     urls = [
-        ('^/(.*)$', i2maps.projects_directory + 'views/'),
-        ('^/media/(.*)$', i2maps.path + 'media/'),
-        ('^/$', i2maps.path + 'media/index.html'),
+        ('^/(.*)$', views_directory),
+        ('^/media/(.*)$', media_directory),
+        ('^/$', media_directory + 'index.html'),
     ]
     
     pico.static_url_map = urls
-    pico.main()
-
+    
+    if len(sys.argv) > 1 and sys.argv[1] == 'shell':
+        embed()
+    else:
+        pico.main()
 
 if __name__ == '__main__':
     run()
